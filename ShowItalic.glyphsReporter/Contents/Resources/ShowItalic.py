9f900b8f9cb4a43ebed1dfe4986d6a2018528b65
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2016-01-02 14:17:29 +0100

    Italic glyphs now centered. Defaulting to unsuffixed glyph if suffixed glyph cannot be found.

--- ShowItalic.glyphsReporter/Contents/Resources/ShowItalic.py
+++ ShowItalic.glyphsReporter/Contents/Resources/ShowItalic.py
@@ -33,26 +33,72 @@ class ShowItalic(ReporterPluginItalic):
 		except Exception as e:
 			self.logToConsole( "italicFontForFont: %s" % str(e) )
 	
+	def shiftLayer( self, thisLayer, xOffset ):
+		try:
+			xShift = NSAffineTransform.transform()
+			xShift.translateXBy_yBy_( xOffset, 0.0 )
+			thisLayer.transform_checkForSelection_( xShift, False )
+		except Exception as e:
+			self.logToConsole( "shiftLayer: %s" % str(e) )
+	
 	def drawBackground(self, layer):
+		# set the default color:
+		drawingColor = NSColor.magentaColor()
+		
+		exactCounterpartShown = True
+		
+		# current font:
 		uprightFont = layer.parent.parent
+		
+		# find the italic/upright counterpart for the font:
 		italicFont = self.italicFontForFont(uprightFont)
 		if italicFont:
 			glyph = layer.parent
 			if glyph:
 				glyphName = glyph.name
+				
+				# find the same glyph in the other font:
 				italicGlyph = italicFont.glyphs[glyphName]
+				
+				# default to unsuffixed version of glyph if exact name does not exist:
+				if not italicGlyph and "." in glyphName:
+					glyphNameWithoutSuffix = glyphName[:glyphName.find(".")]
+					italicGlyph = italicFont.glyphs[glyphNameWithoutSuffix]
+					exactCounterpartShown = False
+					# change color, so user knows it is not an exact counterpart:
+					drawingColor = NSColor.greenColor()
+				
 				if italicGlyph:
+					
+					# determine the same master in other font:
 					italicMaster = None
 					uprightMasterID = layer.associatedMasterId
 					if uprightMasterID:
 						uprightMasterName = uprightFont.masters[uprightMasterID].name.replace("Italic","").strip()
-						print uprightMasterName
 						italicMasters = [m for m in italicFont.masters if uprightMasterName in m.name]
 						if italicMasters:
 							italicMaster = italicMasters[0]
+					
+					# default to the first master:
 					if not italicMaster:
 						italicMaster = italicFont.masters[0]
+					
+					# find the glyph layer that correspnds to the master:
 					italicLayer = italicGlyph.layers[italicMaster.id]
 					if italicLayer:
-						NSColor.magentaColor().set()
-						italicLayer.copyDecomposedLayer().bezierPath().fill()
+						displayLayer = italicLayer.copyDecomposedLayer()
+						
+						# center layer:
+						widthDifference = layer.width - displayLayer.width
+						if widthDifference:
+							self.shiftLayer( displayLayer, widthDifference/2.0 )
+							
+						# draw the layer on the canvas:
+						drawingColor.set()
+						displayLayer.bezierPath().fill()
+						
+						# display info if a different glyph is shown
+						if not exactCounterpartShown:
+							text = " \n \n \n \n%s not found.\nDisplaying %s instead." % ( glyphName, glyphNameWithoutSuffix )
+							textPosition = NSPoint( displayLayer.bounds.origin.x+displayLayer.bounds.size.width/2.0, displayLayer.bounds.origin.y )
+							self.drawTextAtPoint( text, textPosition, fontSize=10.0, fontColor=drawingColor, align='center')
