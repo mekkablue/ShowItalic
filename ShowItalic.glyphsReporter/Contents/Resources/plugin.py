0ad9c5024f0a417cafd531e4e1b9dd2edd7c1d41
schriftgestalt <georg.seifert@mac.com>
2024-07-25 08:01:09 -0700

    cleanup

--- ShowItalic.glyphsReporter/Contents/Resources/plugin.py
+++ ShowItalic.glyphsReporter/Contents/Resources/plugin.py
@@ -1,9 +1,11 @@
 # encoding: utf-8
 from __future__ import division, print_function, unicode_literals
 import objc
-from GlyphsApp import *
-from GlyphsApp.plugins import *
+from Cocoa import NSAffineTransform, NSAffineTransformStruct, NSBezierPath, NSColor, NSPoint, NSControlKeyMask, NSAlternateKeyMask, NSCommandKeyMask, NSClassFromString
+from GlyphsApp import Glyphs, GSAnchor
+from GlyphsApp.plugins import ReporterPlugin
 from math import tan, radians
+
 
 class ShowItalic(ReporterPlugin):
 
@@ -20,48 +22,48 @@ class ShowItalic(ReporterPlugin):
 		self.keyboardShortcut = 'i'
 		self.keyboardShortcutModifier = NSControlKeyMask | NSAlternateKeyMask | NSCommandKeyMask
 		self.threshold = 0.1
-	
+
 	@objc.python_method
-	def foreground( self, layer ):
-		if not self.getScale()>self.threshold and self.conditionsAreMetForDrawing():
+	def foreground(self, layer):
+		if not self.getScale() > self.threshold and self.conditionsAreMetForDrawing():
 			self.drawItalic(layer, shouldFill=False, shouldFallback=False, canShowBounds=False)
-	
+
 	@objc.python_method
 	def background(self, layer):
-		if self.getScale()>self.threshold and self.conditionsAreMetForDrawing():
+		if self.getScale() > self.threshold and self.conditionsAreMetForDrawing():
 			self.drawItalic(layer)
 
 	@objc.python_method
 	def inactiveLayerForeground(self, layer):
-		if self.getScale()>self.threshold and self.conditionsAreMetForDrawing:
+		if self.getScale() > self.threshold and self.conditionsAreMetForDrawing:
 			activeLayer = self.controller.activeLayer()
 			if activeLayer and activeLayer.parent.name in layer.componentNames():
 				for item in activeLayer.selection:
-					if type(item)==GSAnchor:
+					if issubclass(item, GSAnchor):
 						self.drawItalic(layer, shouldFill=False, shouldFallback=True, canShowBounds=True)
 						break
-	
+
 	@objc.python_method
 	def inactiveLayerBackground(self, layer):
-		if self.getScale()>self.threshold and Glyphs.defaults["com.mekkablue.ShowItalic.drawItalicsForInactiveGlyphs"]:
+		if self.getScale() > self.threshold and Glyphs.defaults["com.mekkablue.ShowItalic.drawItalicsForInactiveGlyphs"]:
 			self.drawItalic(layer, shouldFill=False, shouldFallback=False, canShowBounds=(not self.conditionsAreMetForDrawing()))
-	
+
 	@objc.python_method
 	def conditionsAreMetForDrawing(self):
 		"""
 		Don't activate if text or pan (hand) tool are active.
 		"""
-		currentController = self.controller.view().window().windowController()
-		if currentController:
-			tool = currentController.toolDrawDelegate()
-			# textToolIsActive = tool.isKindOfClass_( NSClassFromString("GlyphsToolText") )
-			handToolIsActive = tool.isKindOfClass_( NSClassFromString("GlyphsToolHand") )
-			if not handToolIsActive: 
+		windowController = self.controller.view().window().windowController()
+		if windowController:
+			tool = windowController.toolDrawDelegate()
+			# textToolIsActive = tool.isKindOfClass_(NSClassFromString("GlyphsToolText"))
+			handToolIsActive = tool.isKindOfClass_(NSClassFromString("GlyphsToolHand"))
+			if not handToolIsActive:
 				return True
 		return False
-	
+
 	@objc.python_method
-	def masterHasItalicAngle( self, thisMaster ):
+	def masterHasItalicAngle(self, thisMaster):
 		try:
 			if abs(thisMaster.italicAngle) < 0.01:
 				return False
@@ -70,13 +72,13 @@ class ShowItalic(ReporterPlugin):
 		except:
 			import traceback
 			print(traceback.format_exc())
-	
+
 	@objc.python_method
-	def italicFontForFont( self, thisFont ):
+	def italicFontForFont(self, thisFont):
 		try:
 			if thisFont:
 				familyName = thisFont.familyName
-				listOfPossibleFamilyMembers = [ f for f in Glyphs.fonts if f.familyName == familyName and f != thisFont ]
+				listOfPossibleFamilyMembers = [f for f in Glyphs.fonts if f.familyName == familyName and f != thisFont]
 				if len(listOfPossibleFamilyMembers) == 1:
 					return listOfPossibleFamilyMembers[0]
 				else:
@@ -90,7 +92,7 @@ class ShowItalic(ReporterPlugin):
 			print(e)
 			import traceback
 			print(traceback.format_exc())
-	
+
 	@objc.python_method
 	def transform(self, shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
 		"""
@@ -112,7 +114,7 @@ class ShowItalic(ReporterPlugin):
 		if scale != 1.0:
 			myTransform.scaleBy_(scale)
 		if not (shiftX == 0.0 and shiftY == 0.0):
-			myTransform.translateXBy_yBy_(shiftX,shiftY)
+			myTransform.translateXBy_yBy_(shiftX, shiftY)
 		if skew:
 			skewStruct = NSAffineTransformStruct()
 			skewStruct.m11 = 1.0
@@ -122,31 +124,31 @@ class ShowItalic(ReporterPlugin):
 			skewTransform.setTransformStruct_(skewStruct)
 			myTransform.appendTransform_(skewTransform)
 		return myTransform
-	
+
 	@objc.python_method
-	def drawLine( self, start=NSPoint(), end=NSPoint(), dashed=False ):
+	def drawLine(self, start=NSPoint(), end=NSPoint(), dashed=False):
 		try:
 			line = NSBezierPath.bezierPath()
-			line.moveTo_( start )
-			line.lineTo_( end )
-			line.setLineWidth_( 1.0/self.getScale() )
+			line.moveTo_(start)
+			line.lineTo_(end)
+			line.setLineWidth_(1.0 / self.getScale())
 			if dashed:
-				line.setLineDash_count_phase_((1.0/self.getScale(), 3.0/self.getScale()), 2, 0)
+				line.setLineDash_count_phase_((1.0 / self.getScale(), 3.0 / self.getScale()), 2, 0)
 			line.stroke()
 		except Exception as e:
-			self.logToConsole( "drawLine: %s" % str(e) )
-	
+			self.logToConsole("drawLine: %s" % str(e))
+
 	@objc.python_method
-	def drawHeightSnapsForLayers( self, otherLayer, currentLayer ):
+	def drawHeightSnapsForLayers(self, otherLayer, currentLayer):
 		try:
 			otherHeight = otherLayer.bounds.size.height
 			currentHeight = currentLayer.bounds.size.height
-			
+
 			# only draw if there is something in BOTH layers:
-			if otherHeight != 0 and currentHeight !=0:
-				
+			if otherHeight != 0 and currentHeight != 0:
+
 				# measurements:
-				otherShift = (currentLayer.width-otherLayer.width)*0.5
+				otherShift = (currentLayer.width - otherLayer.width) * 0.5
 				otherLeft = otherLayer.bounds.origin.x + otherShift
 				currentLeft = currentLayer.bounds.origin.x
 				otherRight = otherLeft + otherLayer.bounds.size.width
@@ -156,35 +158,35 @@ class ShowItalic(ReporterPlugin):
 				otherTop = otherBottom + otherHeight
 				currentTop = currentBottom + currentHeight
 				margin = 10.0
-				
+
 				sameHeightColor = NSColor.blueColor().colorWithAlphaComponent_(0.6)
 				differentHeightColor = NSColor.textColor().colorWithAlphaComponent_(0.25)
-				
+
 				# BOTTOMS
 				if otherBottom == currentBottom:
 					sameHeightColor.set()
 					xMin = min(otherLeft, currentLeft) - margin
 					xMax = max(otherRight, currentRight) + margin
-					self.drawLine( start=NSPoint(xMin, otherBottom), end=NSPoint(xMax, otherBottom) )
+					self.drawLine(start=NSPoint(xMin, otherBottom), end=NSPoint(xMax, otherBottom))
 				else:
 					differentHeightColor.set()
-					self.drawLine( start=NSPoint(otherLeft-margin, otherBottom), end=NSPoint(otherRight+margin, otherBottom), dashed=True )
-					self.drawLine( start=NSPoint(currentLeft-margin, currentBottom), end=NSPoint(currentRight+margin, currentBottom), dashed=True )
-				
+					self.drawLine(start=NSPoint(otherLeft - margin, otherBottom), end=NSPoint(otherRight + margin, otherBottom), dashed=True)
+					self.drawLine(start=NSPoint(currentLeft - margin, currentBottom), end=NSPoint(currentRight + margin, currentBottom), dashed=True)
+
 				# TOPS
 				if otherTop == currentTop:
 					sameHeightColor.set()
 					xMin = min(otherLeft, currentLeft) - margin
 					xMax = max(otherRight, currentRight) + margin
-					self.drawLine( start=NSPoint(xMin, otherTop), end=NSPoint(xMax, otherTop) )
+					self.drawLine(start=NSPoint(xMin, otherTop), end=NSPoint(xMax, otherTop))
 				else:
 					differentHeightColor.set()
-					self.drawLine( start=NSPoint(otherLeft-margin, otherTop), end=NSPoint(otherRight+margin, otherTop), dashed=True )
-					self.drawLine( start=NSPoint(currentLeft-margin, currentTop), end=NSPoint(currentRight+margin, currentTop), dashed=True )
-				
+					self.drawLine(start=NSPoint(otherLeft - margin, otherTop), end=NSPoint(otherRight + margin, otherTop), dashed=True)
+					self.drawLine(start=NSPoint(currentLeft - margin, currentTop), end=NSPoint(currentRight + margin, currentTop), dashed=True)
+
 		except Exception as e:
-			self.logToConsole( "drawHeightSnapsForLayers: %s" % str(e) )
-	
+			self.logToConsole("drawHeightSnapsForLayers: %s" % str(e))
+
 	@objc.python_method
 	def cleanName(self, name):
 		triggerWords = (
@@ -202,30 +204,30 @@ class ShowItalic(ReporterPlugin):
 					name = "Regular"
 				break
 		return name
-	
+
 	@objc.python_method
 	def drawItalic(self, layer, shouldFill=True, shouldFallback=True, canShowBounds=True):
 		# set the default color:
 		drawingColor = NSColor.redColor()
 		exactCounterpartShown = True
-		
+
 		# current font:
 		uprightFont = None
 		if layer:
 			glyph = layer.parent
 			if glyph:
 				uprightFont = glyph.parent
-		
+
 		# find the italic/upright counterpart for the font:
 		italicFont = self.italicFontForFont(uprightFont)
 		if italicFont:
 			glyph = layer.parent
 			if glyph:
 				glyphName = glyph.name
-				
+
 				# find the same glyph in the other font:
 				italicGlyph = italicFont.glyphs[glyphName]
-				
+
 				# default to unsuffixed version of glyph if exact name does not exist:
 				if not italicGlyph and "." in glyphName:
 					glyphNameWithoutSuffix = glyphName[:glyphName.find(".")]
@@ -233,7 +235,7 @@ class ShowItalic(ReporterPlugin):
 					exactCounterpartShown = False
 					# change color, so user knows it is not an exact counterpart:
 					drawingColor = NSColor.greenColor().colorWithAlphaComponent_(0.4)
-				
+
 				if italicGlyph:
 					# determine the same master in other font:
 					# default to the first master:
@@ -242,7 +244,7 @@ class ShowItalic(ReporterPlugin):
 					if not isinstance(uprightMasterID, str):
 						uprightMasterID = layer.associatedMasterId()
 						# background layer: associatedMasterId not wrapped
-						
+
 					if uprightMasterID:
 						uprightMasterName = self.cleanName(uprightFont.masters[uprightMasterID].name)
 						# try to find exact expected name:
@@ -257,48 +259,47 @@ class ShowItalic(ReporterPlugin):
 								if m.id == uprightMasterID:
 									italicMaster = italicFont.masters[i]
 									break
-					
+
 					# find the glyph layer that corresponds to the master:
 					italicLayer = italicGlyph.layers[italicMaster.id]
-					if not italicLayer is None:
+					if italicLayer is not None:
 						displayLayer = italicLayer.completeBezierPath
-						scaleFactor = uprightFont.upm/italicFont.upm
+						scaleFactor = uprightFont.upm / italicFont.upm
 						if scaleFactor != 1.0:
 							scale = self.transform(scale=scaleFactor)
 							displayLayer.transformUsingAffineTransform_(scale)
-							
-							
+
 						# center layer:
 						widthDifference = (layer.width - italicLayer.width) * 0.5 * scaleFactor
 						if widthDifference:
 							horizontalShift = self.transform(shiftX=widthDifference)
 							displayLayer.transformUsingAffineTransform_(horizontalShift)
-						
+
 						# draw height snaps on canvas:
 						try:
-							if canShowBounds: # and Glyphs.versionNumber >= 3.0:
+							if canShowBounds:  # and Glyphs.versionNumber >= 3.0:
 								self.drawHeightSnapsForLayers(italicLayer, layer)
 						except:
 							import traceback
 							print(traceback.format_exc())
 							pass
-						
+
 						# draw the layer on the canvas:
 						if shouldFill:
 							drawingColor.colorWithAlphaComponent_(0.25).set()
 							displayLayer.fill()
 						else:
 							drawingColor.colorWithAlphaComponent_(0.45).set()
-							displayLayer.setLineWidth_(1.5/self.getScale())
+							displayLayer.setLineWidth_(1.5 / self.getScale())
 							displayLayer.stroke()
-						
+
 						# display info if a different glyph is shown
 						if not exactCounterpartShown and shouldFallback:
 							text = f" \n \n \n \n{glyphName} not found.\nDisplaying {glyphNameWithoutSuffix} instead."
 							bounds = displayLayer.bounds
-							if type(bounds) == objc.native_selector:
+							if isinstance(bounds, objc.native_selector):
 								bounds = displayLayer.bounds()
-							textPosition = NSPoint(bounds.origin.x+bounds.size.width/2.0, bounds.origin.y)
+							textPosition = NSPoint(bounds.origin.x + bounds.size.width / 2.0, bounds.origin.y)
 							self.drawTextAtPoint(text, textPosition, fontSize=10.0, fontColor=drawingColor, align='center')
 
 	@objc.python_method
